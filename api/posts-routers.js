f0747a5c5baada6c6100ea39c4a6fe04a10a82b8
Samuel Lee <samuelxlee@gmail.com>
2021-01-12 15:27:53 -0800

    wrote endpoints for POSTing to posts api, getting comments array under a post id, and POSTing a comment to said post id

--- api/posts-routers.js
+++ api/posts-routers.js
@@ -29,3 +29,49 @@ router.get("/api/posts/:id", (req, res) => {
         });
 });
 
+router.post("/api/posts", (req, res) => {
+    Post.insert(req.body)
+        .then(post => {
+            if (!post.title || !post.contents) {
+                res.status(400).json({ errorMessage: "Please provide a title and contents for the post." });
+            } else {
+                res.status(201).json(post);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error while saving the post to the database." });
+        });
+});
+
+router.get("api/posts/:id/comments", (req, res) => {
+    Post.findPostComments(req.params.id)
+        .then(comments => {
+            if (comments.length > 0) {
+                res.status(200).json(comments);
+            } else {
+                res.status(404).json({ message: "The post with that specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The comments information could not be retrieved" });
+        });
+});
+
+router.post("api/posts/:id/comments", (req, res) => {
+    Post.insertComment(req.body)
+        .then(comment => {
+            if (!comment) {
+                res.status(404).json({ message: "The post with that specified ID does not exist." });
+            }
+            if (!comment.text) {
+                res.status(400).json({ errorMessage: "Please provide text for the comment." });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error while saving the comment to the database." });
+        });
+});
+
