979a30f0474842f0489da7bc02d8495fcd0045f7
Samuel Lee <samuelxlee@gmail.com>
2021-01-12 16:11:44 -0800

    added endpoints for updating a post and deleting a post

--- api/posts-routers.js
+++ api/posts-routers.js
@@ -44,7 +44,7 @@ router.post("/api/posts", (req, res) => {
         });
 });
 
-router.get("api/posts/:id/comments", (req, res) => {
+router.get("/api/posts/:id/comments", (req, res) => {
     Post.findPostComments(req.params.id)
         .then(comments => {
             if (comments.length > 0) {
@@ -59,7 +59,7 @@ router.get("api/posts/:id/comments", (req, res) => {
         });
 });
 
-router.post("api/posts/:id/comments", (req, res) => {
+router.post("/api/posts/:id/comments", (req, res) => {
     Post.insertComment(req.body)
         .then(comment => {
             if (!comment) {
@@ -67,6 +67,8 @@ router.post("api/posts/:id/comments", (req, res) => {
             }
             if (!comment.text) {
                 res.status(400).json({ errorMessage: "Please provide text for the comment." });
+            } else {
+                res.status(201).json(comment);
             }
         })
         .catch(err => {
@@ -75,3 +77,42 @@ router.post("api/posts/:id/comments", (req, res) => {
         });
 });
 
+router.put("/api/posts/:id", (req, res) => {
+    const changes = req.body;
+
+    Post.update(req.params.id, changes)
+        .then(post => {
+            if (!post) {
+                res.status(404).json({ message: "The post with that specified ID does not exist." });
+            }
+            if (!changes.title || !changes.contents) {
+                res.status(400).json({ errorMessage: "Please provide a title and contents for the post." });
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post information could not be modified." });
+        });
+});
+
+router.delete("/api/posts/:id", (req, res) => {
+    const postToDeleteId = req.params.id;
+    const postToDelete = Post.findById(postToDeleteId);
+
+    Post.remove(postToDeleteId)
+        .then(deletedPost => {
+            if (!deletedPost) {
+                res.status(404).json({ message: "The post with that specified ID does not exist." });
+            } else {
+                res.status(200).json(postToDelete);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post could not be removed." });
+        });
+});
+
+module.exports = router;
